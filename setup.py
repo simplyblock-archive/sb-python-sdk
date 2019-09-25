d2ab2b0dcc3918f4355fd637650d8fb78836cd93
Jitender Bhutani <Jitender.Bhutani@syncrasytech.com>
2019-09-25 18:50:43 +0530

    removed unknown pacakges

--- setup.py
+++ setup.py
@@ -2,20 +2,17 @@ from distutils.core import setup
 setup(
   name='simply_block',
   packages=['simply_block'],
-  version='0.1',
+  version='0.2',
   license='MIT',
   description='Signing and Request package for Simply Block API Gateway',
   author='Jitender Bhutani',
   author_email = 'Jitender.Bhutani21@gmail.com',
   url='https://github.com/JitenderBhutani/simply_block',
-  download_url='https://github.com/JitenderBhutani/simply_block/archive/0.1.tar.gz',
+  download_url='https://github.com/JitenderBhutani/simply_block/archive/0.2.tar.gz',
   keywords=['Simply', 'Block', 'Simply Block', "Blockchain"],   # Keywords that define your package best
   install_requires=[            # TODO
       'requests_toolbelt',
       'requests',
-      'hmac',
-      'json',
-      'hashlib'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
